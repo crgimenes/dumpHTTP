b0da30bd67131b593046bf09c4ca3a0a1dedf130
crgimenes <crg@crg.eti.br>
2024-12-14 18:58:28 -0300

    add parameters

--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"strings"
+	"os/signal"
+	"sync"
 	"time"
 
-	"crg.eti.br/go/config"
-	_ "crg.eti.br/go/config/ini"
 	"github.com/google/uuid"
 )
 
-type Config struct {
-	Port     int    `json:"port" ini:"port" cfg:"port" cfgDefault:"9922"`
-	DumpFile string `json:"dump_file" ini:"dump_file" cfg:"dump_file" cfgDefault:"dump.txt"`
-}
+const (
+	ANSI_RESET         = "\x1b[0m"
+	ANSI_WHITE         = "\x1b[37m"
+	ANSI_BRIGHT_YELLOW = "\x1b[93m"
+	ANSI_BRIGHT_CYAN   = "\x1b[96m"
+)
+
+var (
+	listenAddr *string
+	dumpFile   *string
+	mx         sync.Mutex
+)
 
 func saveDumpToFile(dump []byte) {
-	f, err := os.OpenFile("dump.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(
+		*dumpFile,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644)
 	if err != nil {
 		log.Println(err)
 		return
@@ -28,45 +39,27 @@ func saveDumpToFile(dump []byte) {
 	defer f.Close()
 
 	t := time.Now()
-	uuid := uuid.New()
+	id := uuid.New()
 
-	boundaryStart := fmt.Sprintf("\n- START:%s %s -\n",
-		t.Format("2006-01-02 15:04:05"), uuid.String())
+	boundary := fmt.Sprintf(
+		"\n- boundary-%s-%s\n",
+		t.Format("2006-01-02 15:04:05"),
+		id.String())
 
-	boundaryEnd := fmt.Sprintf("\n- END:%s %s -\n",
-		t.Format("2006-01-02 15:04:05"), uuid.String())
-
-	_, err = f.Write([]byte(boundaryStart))
+	_, err = f.Write([]byte(boundary))
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	// replace all \r\n to \n
-	s := string(dump)
-	s = strings.ReplaceAll(s, "\r\n", "\n")
-	dump = []byte(s)
 
 	_, err = f.Write(dump)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	_, err = f.Write([]byte(boundaryEnd))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	err = f.Sync()
-	if err != nil {
-		log.Println(err)
-		return
-	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	reqDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		log.Println(err)
@@ -74,21 +67,34 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	mx.Lock()
+	defer mx.Unlock()
+
+	fmt.Printf("%sRequest:\n%s%s%s----------------------------------------%s\n",
+		ANSI_BRIGHT_YELLOW,
+		ANSI_WHITE,
+		string(reqDump),
+		ANSI_BRIGHT_CYAN,
+		ANSI_RESET)
+
 	saveDumpToFile(reqDump)
 	w.Write(reqDump)
 }
 
 func main() {
-	cfg := Config{}
+	listenAddr = flag.String("listenAddr", ":8080", "Address to listen")
+	dumpFile = flag.String("dumpFile", "dump.txt", "File to save dump")
+	flag.Parse()
 
-	config.File = "config.ini"
-	err := config.Parse(&cfg)
-	if err != nil {
-		println(err)
-		return
-	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println("\nExiting...")
+		os.Exit(0)
+	}()
 
-	http.HandleFunc("/", indexHandler)
-	log.Printf("Starting dumpHTTP server at port: %d\n", cfg.Port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
+	http.HandleFunc("/", handler)
+	log.Printf("Starting dumpHTTP server on %s\n", *listenAddr)
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
